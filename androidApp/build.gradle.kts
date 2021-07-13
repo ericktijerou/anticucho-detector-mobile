17a2e5039c707701123f7be567bc2bd9e2195a59
Erick Tijero <erick@kanto.co>
2021-07-12 21:52:21 -0500

    Add dependencies as constants

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    compileSdkVersion(AndroidSdk.compile)
+    compileSdk = AndroidSdk.compile
     defaultConfig {
         applicationId = "com.ericktijerou.anticucho_detector.android"
-        minSdkVersion(AndroidSdk.min)
-        targetSdkVersion(AndroidSdk.target)
+        minSdk = AndroidSdk.min
+        targetSdk = AndroidSdk.target
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -56,12 +56,12 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, "androidx.compose.compiler:compiler:${Versions.compose}")
-    implementation("androidx.compose.runtime:runtime:${Versions.compose}")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha02")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-alpha02")
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
+    add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, Compose.compiler)
+    implementation(Compose.runtime)
+    implementation(Android.material)
+    implementation(Lifecycle.runtime)
+    implementation(Lifecycle.viewModel)
+    implementation(Activity.compose)
     implementation(Compose.ui)
     implementation(Compose.uiGraphics)
     implementation(Compose.uiTooling)
@@ -74,14 +74,13 @@ dependencies {
     implementation(Koin.android)
     implementation(Koin.compose)
     implementation(Lifecycle.liveData)
-    val cameraxVersion = "1.0.0"
-    implementation("androidx.camera:camera-camera2:$cameraxVersion")
-    implementation ("androidx.camera:camera-lifecycle:$cameraxVersion")
-    implementation( "androidx.camera:camera-view:1.0.0-alpha26")
-    implementation("org.osmdroid:osmdroid-android:6.1.10")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.test:core:1.4.0")
-    testImplementation("org.robolectric:robolectric:4.5.1")
-    androidTestImplementation("androidx.test:runner:1.4.0")
+    implementation(CameraX.camera2)
+    implementation (CameraX.lifecycle)
+    implementation(CameraX.cameraView)
+    implementation(Android.osmdroid)
+    testImplementation(Test.junit)
+    testImplementation(Test.androidCore)
+    testImplementation(Test.robolectric)
+    androidTestImplementation(Test.androidRunner)
     implementation(project(":shared"))
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,18 +6,21 @@ object Versions {
     const val koin = "3.1.2"
     const val sqlDelight = "1.5.0"
     const val kermit = "0.1.9"
-
     const val sqliteJdbcDriver = "3.30.1"
     const val slf4j = "1.7.30"
     const val compose = "1.0.0-rc01"
     const val nav_compose = "2.4.0-alpha04"
     const val accompanist = "0.13.0"
-
-    const val junit = "4.13"
-    const val testRunner = "1.3.0"
-
+    const val junit = "4.13.2"
+    const val activityCompose = "1.3.0-rc01"
     const val lifecycle = "2.4.0-alpha02"
     const val composeViewModel = "1.0.0-alpha01"
+    const val material = "1.4.0"
+    const val osmdroid = "6.1.10"
+    const val cameraxVersion = "1.0.0"
+    const val cameraView = "1.0.0-alpha26"
+    const val robolectric = "4.5.1"
+    const val androidTest = "1.4.0"
 }
 
 object AndroidSdk {
@@ -32,6 +35,9 @@ object Deps {
 
 object Test {
     const val junit = "junit:junit:${Versions.junit}"
+    const val robolectric = "org.robolectric:robolectric:${Versions.robolectric}"
+    const val androidCore = "androidx.test:core:${Versions.androidTest}"
+    const val androidRunner = "androidx.test:runner:${Versions.androidTest}"
 }
 
 object Compose {
@@ -41,8 +47,19 @@ object Compose {
     const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
     const val material = "androidx.compose.material:material:${Versions.compose}"
     const val navigation = "androidx.navigation:navigation-compose:${Versions.nav_compose}"
-    const val accompanist= "com.google.accompanist:accompanist-coil:${Versions.accompanist}"
+    const val accompanist = "com.google.accompanist:accompanist-coil:${Versions.accompanist}"
     const val composeLifecycle = "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
+    const val runtime = "androidx.compose.runtime:runtime:${Versions.compose}"
+    const val compiler = "androidx.compose.compiler:compiler:${Versions.compose}"
+}
+
+object Activity {
+    const val compose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+}
+
+object Android {
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val osmdroid = "org.osmdroid:osmdroid-android:${Versions.osmdroid}"
 }
 
 object Lifecycle {
@@ -50,40 +67,48 @@ object Lifecycle {
     const val extensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
     const val process = "androidx.lifecycle:lifecycle-process:${Versions.lifecycle}"
     const val compiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
-    const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModel}"
+    const val viewModelCompose =
+        "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModel}"
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
     const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
 }
 
+object CameraX {
+    const val camera2 = "androidx.camera:camera-camera2:${Versions.cameraxVersion}"
+    const val lifecycle = "androidx.camera:camera-lifecycle:${Versions.cameraxVersion}"
+    const val cameraView = "androidx.camera:camera-view:${Versions.cameraView}"
+}
+
 object Koin {
-    val core = "io.insert-koin:koin-core:${Versions.koin}"
-    val test = "io.insert-koin:koin-test:${Versions.koin}"
-    val android = "io.insert-koin:koin-android:${Versions.koin}"
-    val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+    const val core = "io.insert-koin:koin-core:${Versions.koin}"
+    const val test = "io.insert-koin:koin-test:${Versions.koin}"
+    const val android = "io.insert-koin:koin-android:${Versions.koin}"
+    const val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
 }
 
 object Ktor {
-    val clientCore = "io.ktor:ktor-client-core:${Versions.ktor}"
-    val clientJson = "io.ktor:ktor-client-json:${Versions.ktor}"
-    val clientLogging = "io.ktor:ktor-client-logging:${Versions.ktor}"
-    val clientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
-    val clientAndroid = "io.ktor:ktor-client-android:${Versions.ktor}"
-    val clientApache = "io.ktor:ktor-client-apache:${Versions.ktor}"
-    val slf4j = "org.slf4j:slf4j-simple:${Versions.slf4j}"
-    val clientIos = "io.ktor:ktor-client-ios:${Versions.ktor}"
-    val clientCio = "io.ktor:ktor-client-cio:${Versions.ktor}"
-    val clientJs = "io.ktor:ktor-client-js:${Versions.ktor}"
+    const val clientCore = "io.ktor:ktor-client-core:${Versions.ktor}"
+    const val clientJson = "io.ktor:ktor-client-json:${Versions.ktor}"
+    const val clientLogging = "io.ktor:ktor-client-logging:${Versions.ktor}"
+    const val clientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
+    const val clientAndroid = "io.ktor:ktor-client-android:${Versions.ktor}"
+    const val clientApache = "io.ktor:ktor-client-apache:${Versions.ktor}"
+    const val slf4j = "org.slf4j:slf4j-simple:${Versions.slf4j}"
+    const val clientIos = "io.ktor:ktor-client-ios:${Versions.ktor}"
+    const val clientCio = "io.ktor:ktor-client-cio:${Versions.ktor}"
+    const val clientJs = "io.ktor:ktor-client-js:${Versions.ktor}"
 }
 
 object Serialization {
-    val core = "org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}"
+    const val core = "org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}"
 }
 
 object SqlDelight {
-    val runtime = "com.squareup.sqldelight:runtime:${Versions.sqlDelight}"
-    val coroutineExtensions = "com.squareup.sqldelight:coroutines-extensions:${Versions.sqlDelight}"
-    val androidDriver = "com.squareup.sqldelight:android-driver:${Versions.sqlDelight}"
-    val nativeDriver = "com.squareup.sqldelight:native-driver:${Versions.sqlDelight}"
-    val nativeDriverMacos = "com.squareup.sqldelight:native-driver-macosx64:${Versions.sqlDelight}"
-    val jdbcDriver = "org.xerial:sqlite-jdbc:${Versions.sqliteJdbcDriver}"
-    val sqlliteDriver = "com.squareup.sqldelight:sqlite-driver:${Versions.sqlDelight}"
+    const val runtime = "com.squareup.sqldelight:runtime:${Versions.sqlDelight}"
+    const val coroutineExtensions = "com.squareup.sqldelight:coroutines-extensions:${Versions.sqlDelight}"
+    const val androidDriver = "com.squareup.sqldelight:android-driver:${Versions.sqlDelight}"
+    const val nativeDriver = "com.squareup.sqldelight:native-driver:${Versions.sqlDelight}"
+    const val nativeDriverMacos = "com.squareup.sqldelight:native-driver-macosx64:${Versions.sqlDelight}"
+    const val jdbcDriver = "org.xerial:sqlite-jdbc:${Versions.sqliteJdbcDriver}"
+    const val sqlliteDriver = "com.squareup.sqldelight:sqlite-driver:${Versions.sqlDelight}"
 }
